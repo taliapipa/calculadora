36ae1f9c2740555cb5457b6a19bd5809d4b8c768
taliapipa <125282306+taliapipa@users.noreply.github.com>
2023-08-07 14:57:50 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,58 @@
+import '../styles/App.css';
+
+
+function App() {
+
+  return (
+    <div className="Calculadora">
+      <div>
+      <button>
+          AC
+        </button>
+        <button>
+          +/-
+        </button>
+        <button>
+          %
+        </button>
+        <button>
+          DIV
+        </button>
+      </div>
+      <div className="números">
+        <button>
+          0
+        </button>
+        <button>
+          1
+        </button>
+        <button>
+          2
+        </button>
+        <button>
+          3
+        </button>
+        <button>
+          4
+        </button>
+        <button>
+          5
+        </button>
+        <button>
+          6
+        </button>
+        <button>
+          7
+        </button>
+        <button>
+          8
+        </button>
+        <button>
+          9
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
